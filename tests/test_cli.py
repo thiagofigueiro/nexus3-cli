851f748980d918287d85a97f02b1a6f2bcafee3e
Thiago Figueiró <thiagocsf@gmail.com>
2018-06-21 08:09:54 +1000

    test: repo create hosted

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,6 @@
+import itertools
+import pytest
+
 import nexuscli
 
 
@@ -21,3 +24,24 @@ def test_repo_list(mocker):
     nexuscli.cli.get_client.assert_called_once()
     nexuscli.cli.cmd_repo_do_list.assert_called_with(
         nexuscli.cli.get_client.return_value)
+
+
+@pytest.mark.parametrize(
+    'repo_format, w_policy, strict', itertools.product(
+        ['npm', 'pypi', 'raw', 'rubygems', 'yum'],  # format
+        list(nexuscli.cli.NexusClient.POLICIES['write']),  # w_policy
+        ['', '--strict-content'],  # strict
+    ))
+@pytest.mark.integration
+def test_repo_create_hosted(repo_format, w_policy, strict, nexus_client):
+    strict_name = strict[2:8]
+    repo_name = 'hosted-{repo_format}-{w_policy}-{strict_name}'.format(
+        **locals())
+    argv = ('repo create hosted {repo_format} {repo_name} '
+            '--write={w_policy} {strict}'.format(**locals())).split(' ')
+
+    nexuscli.cli.main(argv=list(filter(None, argv)))
+
+    repositories = nexus_client.repo_list()
+
+    assert any(r['name'] == repo_name for r in repositories)
