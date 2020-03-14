a3308022c131e2e50b0137511e8ab7d8d48fff7c
Thiago Figueiro <thiagocsf@gmail.com>
2020-03-15 09:47:32 +1100

    Increase nexus timeout in integration tests

--- tests/wait-for-nexus.sh
+++ tests/wait-for-nexus.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 function nexus_ready {
-  [[ "200" == $(curl -o /dev/null -s -w "%{http_code}\n" localhost:8081) ]]
+  [[ "200" == $(curl -o /dev/null -s -w "%{http_code}\n" nexus:8081) ]]
 }
 
 count=0
 until nexus_ready
 do
   count=$((count+1))
-  if [ ${count} -gt 70 ]
+  if [ ${count} -gt 60 ]
   then
     echo 'Timeout-out waiting for nexus container'
     exit 1
