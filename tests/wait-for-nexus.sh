c644850f095c073d8954406e36a7ab8be2c1d366
Thiago Figueiro <thiagocsf@gmail.com>
2020-03-15 11:07:12 +1100

    Use DIND for integration tests

--- tests/wait-for-nexus.sh
+++ tests/wait-for-nexus.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
+
 function nexus_ready {
-  [[ "200" == $(curl -o /dev/null -s -w "%{http_code}\n" nexus:8081) ]]
+  [[ "200" == $(curl -o /dev/null -s -w "%{http_code}\n" "$1") ]]
 }
 
 count=0
-until nexus_ready
+until nexus_ready "${1:-http://localhost:8081}"
 do
   count=$((count+1))
-  if [ ${count} -gt 60 ]
+  if [ ${count} -gt 100 ]
   then
     echo 'Timeout-out waiting for nexus container'
+    docker logs --tail 50 nexus
+    docker ps
+    curl -sv "%{http_code}\n" "$1"
+    netstat -ntlp
     exit 1
   fi
-  sleep 1
+  sleep 3
 done
